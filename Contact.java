930c0644ad5d9b5b88265b7f483934a9ac15409f
Yash <yashshah@trentu.ca>
2024-02-15 20:47:25 -0500

    Created listContacts,removeContacts and addContacts, and added getters and setters

--- Contact.java
+++ Contact.java
@@ -28,4 +28,29 @@ class Contact {
 
     // Getters and Setters (if necessary)
     // ...
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+
 }
--- ContactManager.java
+++ ContactManager.java
@@ -15,16 +15,35 @@ class ContactManager {
     // Method to add a new Contact
     public void addContact(Contact contact) {
         // Your code here
+        contacts.add(contact);
     }
 
     // Method to remove a Contact by name
     public void removeContact(String name) {
         // Your code here
+        Iterator<Contact> iterator = contacts.iterator();
+        while (iterator.hasNext()) {
+            Contact contact = iterator.next();
+            if (contact.getName().equals(name)){
+                iterator.remove();
+                System.out.println("Contact " + name + " has been Removed");
+                System.out.println("___________________________________________");
+                return;
+            }
+            
+        }
+        System.out.println("Contact cannot be found");
     }
 
     // Method to list all Contacts
     public void listContacts() {
         // Your code here
+        for(Contact contact : contacts){
+        System.out.println("Name: " + contact.getName() + "\n" +
+            "Email: " + contact.getEmail() + "\n" +
+            "Phone Number: " + contact.getPhoneNumber() + "\n" +
+            "___________________________________________");
+        }
     }
 
 
